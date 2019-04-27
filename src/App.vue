0acb8dadcc39b46827140d3150c291ec1aa93c79
Tim Pfeifer <timpfeifer@mailbox.org>
2019-04-27 14:08:06 +0200

    sidebar added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
-  <div id="app">
-    <Map></Map>
-  </div>
+    <div class="app">
+        <Sidebar class="sidebar"></Sidebar>
+        <div class="content">
+            <Map class="map"></Map>
+        </div>
+    </div>
 </template>
 
 <script>
-import Map from './components/Map.vue'
+  import Map from './components/Map.vue';
+  import Sidebar from './components/Sidebar';
 
-export default {
-  name: 'app',
-  components: {
-    Map
+  export default {
+    name: 'app',
+    components: {
+      Map,
+      Sidebar
+    }
   }
-}
 </script>
 
 <style>
+    .sidebar {
+        margin: 0;
+        padding: 0;
+        width: 200px;
+        background-color: #fff;
+        position: fixed;
+        height: 100%;
+        overflow: auto;
+    }
+
+    .content {
+        margin-left: 200px;
+    }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="sidebar">
+        <ul>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Sidebar"
+  }
+</script>
+
+<style scoped>
+
+</style>
