d052afa8e9327c2f4acb7d4a92cc399324b91eb5
Tim Pfeifer <timpfeifer@mailbox.org>
2019-04-27 16:26:00 +0200

    about, favorites and profile page implemented

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="favorites">
+        <h1>Favoriten</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Favorites"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -2,11 +2,17 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Index from './pages/Index';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 Vue.use(VueRouter);
 
 const router = [
-  { path: '/', component: Index, props: true}
+  { path: '/', component: Index, props: true},
+  { path: '/favorites', component: Favorites, props: true},
+  { path: '/profile', component: Profile, props: true},
+  { path: '/about', component: About, props: true}
 ];
 
 
